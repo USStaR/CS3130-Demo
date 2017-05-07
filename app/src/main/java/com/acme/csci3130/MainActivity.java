1271b660a4514c09e61cb011d6d5a78c49d03ed3
Sasha Star <alexandrosh22@gmail.com>
2017-05-07 16:28:43 -0300

    Made UI

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,29 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.helloText);
+        final EditText simpleEditText = (EditText) findViewById(R.id.editText);
+        simpleEditText.setHint("Enter Your Name Here");//display the hint
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextValue = simpleEditText.getText().toString();
+                textView.setText(editTextValue);
+            }
+        });
     }
 }
